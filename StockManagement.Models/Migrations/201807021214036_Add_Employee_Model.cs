f7e13e71173391ceddde393efb29c76c7f3aab49
shirajulmamun <shirajul.mamun@gmail.com>
2018-07-02 18:15:41 +0600

    [Migration] Employee Model Added

---
+++ StockManagement.Models/Migrations/201807021214036_Add_Employee_Model.cs
@@ -0,0 +1,28 @@
+namespace StockManagement.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Employee_Model : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Address = c.String(),
+                        Designation = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
